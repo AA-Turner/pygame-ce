a7c1ad2d91b3b3a0dea3fa576ef8156ca9e456b1
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2023-08-25 04:39:53 +0100

    tmp

--- docs/reST/conf.py
+++ docs/reST/conf.py
@@ -2,23 +2,15 @@ import sys, os
 sys.path.append(os.path.abspath('.'))
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
     'ext.indexer',
     'ext.headers',
     'ext.boilerplate',
 ]
-project = 'pygame-ce'
-copyright = '2000-2022, pygame developers, 2023 pygame-ce developers'
-version = release = '2.4.0'
+templates_path = ['_templates']
 # Format strings for the version directives
 add_module_names = True
 modindex_common_prefix = ['pygame']
 boilerplate_skip_transform = ['index']
-html_theme = 'classic'
-html_theme_options = {'home_uri': 'https://pyga.me/docs'}
-html_theme_path = ['themes']
-html_static_path = ['_static']
-html_extra_path = ['../LGPL.txt']
 html_use_modindex = False
 html_show_sphinx = False
 smartquotes = False
